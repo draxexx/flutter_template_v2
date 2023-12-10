4e0120db651061782951f2abb7fb718976897f2f
Burak <burak.yilmaz@simdatix.com>
2023-12-10 17:59:13 +0300

    create gen package

---
+++ module/gen/lib/src/environment/dev_env.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dev_env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _DevEnv {
+  static const List<int> _enviedkey_baseUrl = <int>[
+    761447480,
+    2716740494,
+    734115859,
+    3781458266,
+    3701548452,
+    3301363232,
+    3197243789,
+    4238888230,
+    2999103173,
+    1707137866,
+    3157640694,
+    3539742025,
+    2860276683,
+    2035900190,
+    575280472,
+    2845494542,
+    928277344,
+    3116321306,
+    2013338057,
+    2768679602,
+    1768628654,
+  ];
+
+  static const List<int> _envieddata_baseUrl = <int>[
+    761447504,
+    2716740602,
+    734115943,
+    3781458218,
+    3701548446,
+    3301363215,
+    3197243810,
+    4238888266,
+    2999103146,
+    1707137833,
+    3157640599,
+    3539741989,
+    2860276643,
+    2035900273,
+    575280427,
+    2845494650,
+    928277338,
+    3116321321,
+    2013338105,
+    2768679554,
+    1768628638,
+  ];
+
+  static final String _baseUrl = String.fromCharCodes(List<int>.generate(
+    _envieddata_baseUrl.length,
+    (int i) => i,
+    growable: false,
+  ).map((int i) => _envieddata_baseUrl[i] ^ _enviedkey_baseUrl[i]));
+}
---
+++ module/gen/lib/src/environment/prod_env.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'prod_env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _ProdEnv {
+  static const List<int> _enviedkey_baseUrl = <int>[
+    3892100571,
+    1573489142,
+    794760939,
+    2478542554,
+    588131989,
+    2437913618,
+    1888487670,
+    891205521,
+    3855511040,
+    3559796272,
+    3627292894,
+    1072625134,
+    1435561058,
+    516566121,
+    4131925159,
+  ];
+
+  static const List<int> _envieddata_baseUrl = <int>[
+    3892100531,
+    1573489026,
+    794760863,
+    2478542506,
+    588132070,
+    2437913640,
+    1888487641,
+    891205566,
+    3855511137,
+    3559796288,
+    3627292855,
+    1072625088,
+    1435560961,
+    516566022,
+    4131925194,
+  ];
+
+  static final String _baseUrl = String.fromCharCodes(List<int>.generate(
+    _envieddata_baseUrl.length,
+    (int i) => i,
+    growable: false,
+  ).map((int i) => _envieddata_baseUrl[i] ^ _enviedkey_baseUrl[i]));
+}
